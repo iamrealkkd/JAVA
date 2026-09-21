a7f4a32b6da4cf4c400010ab87d10ad11bab0953
Krishna Kumar Dutta <kkrishna.bga@gmail.com>
2026-09-21 19:30:42 +0530

    Valid Date

---
+++ 01_0_BASICS/_01_0_basics.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+class _01_0_basics {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int day = sc.nextInt(), month = sc.nextInt(), year = sc.nextInt();
+   
+        if (year <= 0) {
+            System.out.println("Invalid Date");
+        }
+        else {
+            if (month < 1 || month > 12) {
+                System.out.println("Invalid Date");
+            }
+            else {
+                if (month == 2) {
+
+                    if ((year % 400 == 0) || 
+                        (year % 4 == 0 && year % 100 != 0)) {
+
+                        if (day >= 1 && day <= 29)
+                            System.out.println("Valid Date");
+                        else
+                            System.out.println("Invalid Date");
+
+                    } else {
+
+                        if (day >= 1 && day <= 28)
+                            System.out.println("Valid Date");
+                        else
+                            System.out.println("Invalid Date");
+                    }
+                }
+                else if (month == 4 || month == 6 ||
+                         month == 9 || month == 11) {
+
+                    if (day >= 1 && day <= 30)
+                        System.out.println("Valid Date");
+                    else
+                        System.out.println("Invalid Date");
+                }
+
+                else {
+                    if (day >= 1 && day <= 31)
+                        System.out.println("Valid Date");
+                    else
+                        System.out.println("Invalid Date");
+                }
+            }
+        }
+        sc.close();
+    }
+}
