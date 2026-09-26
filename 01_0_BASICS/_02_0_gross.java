451044ea84c60f6bc7b40131f3f68bd3733d03a6
Krishna Kumar Dutta <kkrishna.bga@gmail.com>
2026-09-26 09:49:48 +0530

    Class Imp

--- 01_0_BASICS/_02_0_gross.java
+++ 01_0_BASICS/_02_0_gross.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class _02_0_gross {
+class _02_0_gross {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         double ba, da, sa, gross = 0;
